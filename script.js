968744ce63e01ec3cfc4fee91d4061a862d537ea
David King <david@oodavid.com>
2013-04-03 13:53:18 +0100

    Tweaked the visual style somehwat (pretty happy now) and started writing the content

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-	// Grab guru's starting position
-	var guruTop   = parseInt($('#guru div').css('top'), 10);
-	var guruRight = parseInt($('#guru').css('marginRight'), 10);
+	// Starting positions
+	var guruTop		= parseInt($('#guru').css('top'), 10);
+	var guruLeft	= parseInt($('#guru').css('left'), 10);
+	var shadowLeft	= parseInt($('#shadow').css('left'), 10);
 	// Counter...
 	var guruDt = 0;
 	// The mover....
@@ -11,9 +12,13 @@ $(document).ready(function() {
 		var dX = Math.sin(guruDt) * 20;
 		var dY = Math.cos(guruDt) * 10;
 		// Apply
-		$('#guru').css('marginRight', (guruRight + dX) + "px");
-		$('#guru div').css('top', (guruTop + dY) + "px");
+		$('#guru').css('top', (guruTop + dY) + "px");
+		$('#guru').css('left', (guruLeft + dX) + "px");
+		$('#shadow').css('left', (shadowLeft + dX) + "px");
 	};
 	// Now go!
 	setInterval(moveGuru, 30);
+	moveGuru();
+	// And render
+	$('#meditate').show();
 });
