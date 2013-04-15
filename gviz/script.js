205f0633dc7ad0096354b03bb7cbf2d6f2fadcc7
David King <david@oodavid.com>
2013-04-15 09:50:36 +0100

    Added helper tool to GraphViz page

---
+++ gviz/script.js
@@ -0,0 +1,23 @@
+// Example:
+$(document).ready(function(){
+	// Grow the textarea
+	$('#tool textarea').autosize();   
+	// When we submit...
+	$('#tool button').click(function(e){
+		// Pull out the values...
+		var layout	= $('#tool select').val();
+		var content	= $('#tool textarea').val();
+		// Create the script STRING and element
+		var string	= '<script type="gviz" data-layout="' + layout + '"><![CDATA[\n' + content + '\n]]></script>';
+		// Show the embeddable HTML
+		var intro	= $('<p>').text('Add this to your page...');
+		var html	= $('<pre>').addClass('brush: xml').text(string);
+		var outro	= $('<p>').text('...and this will be rendered');
+		$('#tool #embed').empty().append(intro).append(html).append(outro);
+		SyntaxHighlighter.highlight();
+		// Add the script and parse it
+		var script	= $(string);
+		$('#tool figure').empty().append(script);
+		gviz.parse(script);
+    });
+});
